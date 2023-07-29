99470fe41210764e5b11aa3f9bb425916ec09938
Pavlos Fragkiadoulakis <57967801+pfragkiad@users.noreply.github.com>
2023-07-29 20:00:22 +0300

    Save 2nd class.

---
+++ MultiBranch/BSecondary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiBranch;
+
+internal class BSecondary
+{
+    public static string Test2 = "ESEC!";
+}
--- MultiBranch/Program.cs
+++ MultiBranch/Program.cs
@@ -5,5 +5,7 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine(BCommon.Test);
+
+        Console.WriteLine(BSecondary.Test2);
     }
 }
