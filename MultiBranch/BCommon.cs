d633f1b066f5d25a787fd8690d359705119ccc65
Pavlos Fragkiadoulakis <57967801+pfragkiad@users.noreply.github.com>
2023-07-29 19:56:37 +0300

    Add project files.

---
+++ MultiBranch/BCommon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiBranch;
+
+public class BCommon
+{
+    public static string Test = "asd";
+}
---
+++ MultiBranch/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiBranch;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(BCommon.Test);
+    }
+}
