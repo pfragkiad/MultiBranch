606c37f6cb16f31e36607babba830c84e1b53404
Pavlos Fragkiadoulakis <57967801+pfragkiad@users.noreply.github.com>
2023-07-29 20:48:44 +0300

    Add appsettings.

--- MultiBranch/Program.cs
+++ MultiBranch/Program.cs
@@ -1,11 +1,21 @@
-﻿namespace MultiBranch;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MultiBranch;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        var app = Host.CreateDefaultBuilder().Build();
+
         Console.WriteLine(BCommon.Test);
 
+        var config = app.Services.GetRequiredService<IConfiguration>();
+        Console.WriteLine(config.GetValue<int>("Test"));
+
         Console.WriteLine(BSecondary.Test2);
+
     }
 }
